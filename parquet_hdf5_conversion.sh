89a5a639ade8773cb3bb955a3229d0148038dae6
Bhim Bam <bbbam@crimson.ua.edu>
2024-11-17 00:15:14 -0800

    convert parquet to h5

--- parquet_hdf5_conversion.sh
+++ parquet_hdf5_conversion.sh
@@ -3,8 +3,9 @@
 #SBATCH -N 1
 #SBATCH -C cpu
 #SBATCH -q regular
-#SBATCH -J slurm_nan_replace
-#SBATCH -t 02:10:00
+#SBATCH -J pq_h5_phy
+#SBATCH --output=slurm_pq_h5_phy_%J.out
+#SBATCH -t 22:10:00
 
 #OpenMP settings:
 export OMP_NUM_THREADS=1
@@ -12,4 +13,4 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
 #run the application:
-srun -n 1 -c 4 --cpu_bind=cores python parquet_hdf5_conversion.py
+srun -n 1 -c 10 --cpu_bind=cores python parquet_hdf5_conversion.py -i /pscratch/sd/b/bbbam/IMG_aToTauTau_Hadronic_m3p6To18_pt30T0300_unbiased -o /pscratch/sd/b/bbbam/IMG_aToTauTau_Hadronic_m3p6To18_pt30T0300_unbiased_h5
